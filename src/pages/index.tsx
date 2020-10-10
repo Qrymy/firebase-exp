c5473e23840ed34cd945bf388a7dcdbb43609b86
Qrymy <regist@qry.mx>
2020-10-10 10:51:39 +0900

    Create components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,45 @@
+import { useEffect, useCallback } from 'react'
 import { NextPage } from 'next'
+import { useAuth } from '@hooks/use-auth'
+import { useFirestore } from '@hooks/use-firestore'
+import { TodoInput } from '@components/todo-input'
+import { Todo } from '@models/todo'
 
-const HomePage: NextPage = () => <div>Init</div>
+const collectionName = 'todos'
+
+const HomePage: NextPage = () => {
+  const { user, signIn } = useAuth()
+
+  const { items, getItems, setDocument } = useFirestore<Todo>()
+
+  useEffect(() => {
+    signIn()
+  }, [])
+
+  useEffect(() => {
+    if (user) {
+      getItems(collectionName)
+    }
+  }, [user])
+
+  const handleSubmit = useCallback(
+    async (content) => {
+      if (user) {
+        await setDocument(collectionName, { content })
+        await getItems(collectionName)
+      }
+    },
+    [user]
+  )
+
+  return (
+    <div>
+      <TodoInput onSubmit={handleSubmit} />
+      {items.map(({ id, content }) => (
+        <div key={id}>{content}</div>
+      ))}
+    </div>
+  )
+}
 
 export default HomePage
